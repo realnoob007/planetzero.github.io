ccb434ce88b40f7b1b3b6d461f4fd7a437f956d9
Sophia Patel <87698941+realnoob007@users.noreply.github.com>
2023-12-28 08:42:34 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ set -e
 npm run docs:build
 
 # 构建Docker镜像 amd64
-docker buildx build --platform linux/amd64 -t xyhelper/xyhelper-www:latest . --push
+docker buildx build --platform linux/amd64 -t xyhelper/planetzero-www:latest . --push
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-echo 'xyhelper.cn' > CNAME
+echo 'planetzero.cn' > CNAME
 
 # git init
 # 初始化 main 分支  
@@ -26,6 +26,6 @@ git commit -m "deploy | $(date +'%Y-%m-%d %H:%M:%S')"
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:xyhelper/xyhelper.github.io.git gh-pages:gh-pages
+git push -f git@github.com:realnoob007/planetzero.github.io.git gh-pages:gh-pages
 
 cd -
