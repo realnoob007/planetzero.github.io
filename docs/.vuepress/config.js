043a72d39d9ae310d89edb7ba4a3358813433f67
Sophia Patel <87698941+realnoob007@users.noreply.github.com>
2023-12-28 09:03:12 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,8 +4,8 @@ module.exports = {
     // 作为特例，默认语言可以使用 '/' 作为其路径。
     "/": {
       lang: "zh-CN", // 将会被设置为 <html> 的 lang 属性
-      title: "XYHELPER",
-      description: "免费的AI助手",
+      title: "Planetzero",
+      description: "全能AI助手",
     },
   },
   themeConfig: {
@@ -14,16 +14,14 @@ module.exports = {
     nav: [
       { text: "首页", link: "/" },
       { text: "使用入口", link: "/ai/" },
-      { text: "小强车队", link: "/xq/" },
+      //{ text: "小强车队", link: "/xq/" },
       // { text: "使用教程", link: "/tutorial/" },
       // { text: "网页版", link: "/web/" },
       // { text: "客户端下载", link: "/download/" },
-      { text: "会员服务", link: "/plus/" },
+      { text: "服务体系", link: "/plus/" },
       // { text: "开发者中心", link: "/developer/" },
-      { text: "专享接入点", link: "/access/" },
-      { text: "关于我们", link: "/about/" },
-      { text: "服务状态", link: "https://uptime.xyhelper.cn/" },
-      { text: "GitHub", link: "https://github.com/xyhelper" },
+      //{ text: "专享接入点", link: "/access/" },
+      { text: "联系客服", link: "/about/" },
     ],
   },
 };
