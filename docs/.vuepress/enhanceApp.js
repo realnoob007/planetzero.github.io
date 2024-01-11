101c5e9ff724e417b433eeb5dbef9ca70fff53e4
Sophia Patel <87698941+realnoob007@users.noreply.github.com>
2024-01-11 15:59:49 +0800

    Update enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
-import VueIframe from "vue-iframe";
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Element)
-Vue.use(VueIframe);
 // export default ({ Vue }) => {
 //     Vue.use(Element)
 // };
